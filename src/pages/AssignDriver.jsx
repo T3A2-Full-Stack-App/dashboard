e061c28d330f9127f3b846dc11f1d478e63b687e
adrianalain <adrian.goportaje@gmail.com>
2022-07-22 15:24:16 +1000

    Update Styling AssignDrivers

--- src/pages/AssignDriver.jsx
+++ src/pages/AssignDriver.jsx
@@ -66,80 +66,95 @@ const AssignDriver = () => {
 
 
     return (
-        <>
-            <div className="flex gap-10 flex-wrap justify-center">
-                <div
-                    className="bg-white dark:text-gray-200
+      <>
+        <div className="flex gap-10 flex-wrap justify-center">
+          <div
+            className="bg-white dark:text-gray-200
               dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780"
-                >
-                    <Header title="Assign Vehicle to Driver" />
-                    <div className="flex justify-between">
-                        <p className="font-semibold text-3xl"></p>
-                        <div className="flex flex-wrap items-center gap-4">
-                        </div>
-                    </div>
+          >
+            <Header title="Assign Vehicle to Driver" />
+            <div className="flex justify-between">
+              <p className="font-semibold text-3xl"></p>
+              <div className="flex flex-wrap items-center gap-4"></div>
+            </div>
 
-                    <label className="tracking-wide text-base font-bold">
-                        Select driver by email:
-                    </label>
-                    <form onSubmit={(e) => pairDriverVehicle(e, thisDriverData, thisVehicleData)}>
-                    <select
-                        required
-                        className="appearance-none font-normal block w-52 bg-gray-200 text-gray-700 border border-gray-300 rounded py-2 px-3 mb-3 leading-tight focus:outline-none focus:bg-white"
-                        id="email"
-                        type="text"
-                        onChange={(e) => getThisDriver(e.target.value)}
-                    >
-                        {driverData.map(driver =>
-                            <option key={driver.email} value={driver.email}>{driver.email}</option>
-                        )}
-                    </select>
+            <label className="tracking-wide text-base font-bold">
+              Select driver by email:
+            </label>
+            <form
+              onSubmit={(e) =>
+                pairDriverVehicle(e, thisDriverData, thisVehicleData)
+              }
+            >
+              <select
+                required
+                className="appearance-none font-normal block w-52 bg-gray-200 text-gray-700 border border-gray-300 rounded py-2 px-3 mb-3 leading-tight focus:outline-none focus:bg-white"
+                id="email"
+                type="text"
+                onChange={(e) => getThisDriver(e.target.value)}
+              >
+                {driverData.map((driver) => (
+                  <option key={driver.email} value={driver.email}>
+                    {driver.email}
+                  </option>
+                ))}
+              </select>
 
-                    <label className="tracking-wide text-base font-bold">
-                        Select vehicle by registration:
-                    </label>
-                    <select
-                        required
-                        className="appearance-none font-normal block w-52 bg-gray-200 text-gray-700 border border-gray-300 rounded py-2 px-3 mb-3 leading-tight focus:outline-none focus:bg-white"
-                        id="registration"
-                        type="text"
-                        onChange={(e) => getThisVehicle(e.target.value)}
-                    >
-                        {vehicleData.map(vehicle =>
-                            <option key={vehicle.registration} value={vehicle.registration}>{vehicle.registration}</option>
-                        )}
-                    </select>
-                    <label className="tracking-wide text-base font-normal">
-                        Vehicle Details
-                    </label>
-                    <ul>
-                        <li>Make: {thisVehicleData.make}</li>
-                        <li>Model: {thisVehicleData.model}</li>
-                        <li>Year: {thisVehicleData.year}</li>
-                        </ul>
-                        
-                            <button className="h-8 px-14  mt-8 text-sm bg-blue-500 hover:bg-blue-700 text-white transition-colors duration-15 rounded-lg focus:shadow-outline"
-                                type="submit"
-                            >
-                                Save
-                            </button>
-                    </form> 
-                    <Link
-                        to="/drivers"
-                        className="flex justify-end font-medium file:mt-2 text-blue-500 hover:text-blue-700 "
-                    >
-                        Back to Driver Lists
-                    </Link>
-                    <Link
-                        to="/vehicles"
-                        className="flex justify-end font-medium file:mt-2 text-blue-500 hover:text-blue-700 "
-                    >
-                        Back to Vehicle Lists
-                    </Link>
-                </div>
-            </div >
-        </>
-    )
+              <label className="tracking-wide text-base font-bold">
+                Select vehicle by registration:
+              </label>
+              <select
+                required
+                className="appearance-none font-normal block w-52 bg-gray-200 text-gray-700 border border-gray-300 rounded py-2 px-3 mb-3 leading-tight focus:outline-none focus:bg-white"
+                id="registration"
+                type="text"
+                onChange={(e) => getThisVehicle(e.target.value)}
+              >
+                {vehicleData.map((vehicle) => (
+                  <option
+                    key={vehicle.registration}
+                    value={vehicle.registration}
+                  >
+                    {vehicle.registration}
+                  </option>
+                ))}
+              </select>
+              <label className="tracking-wide text-base font-normal">
+                Vehicle Details
+              </label>
+              <ul>
+                <li>Make: {thisVehicleData.make}</li>
+                <li>Model: {thisVehicleData.model}</li>
+                <li>Year: {thisVehicleData.year}</li>
+              </ul>
+
+              <button
+                className="h-8 px-14  mt-8 text-sm bg-blue-500 hover:bg-blue-700 text-white transition-colors duration-15 rounded-lg focus:shadow-outline"
+                type="submit"
+              >
+                Save
+              </button>
+            </form>
+            <div className="flex justify-end tracking-wide font-normal text-base  content-around">
+              <Link
+                to="/drivers"
+                className="mr-2 flex justify-end font-medium file:mt-2 text-blue-500 hover:text-blue-700 "
+              >
+                Back to Driver Lists
+              </Link>
+              |
+              <Link
+                to="/vehicles"
+                className="ml-2 flex justify-end font-medium file:mt-2 text-blue-500 hover:text-blue-700 "
+              >
+                {" "}
+                Back to Vehicle Lists
+              </Link>
+            </div>
+          </div>
+        </div>
+      </>
+    );
 }
 
 
