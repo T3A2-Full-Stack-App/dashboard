04cb44ef4fa6b81601e0c6ddd813137a27df3f47
adrianalain <adrian.goportaje@gmail.com>
2022-07-22 13:05:34 +1000

    Styling in Edit Runs

--- src/pages/EditRuns.jsx
+++ src/pages/EditRuns.jsx
@@ -78,25 +78,29 @@ const EditRuns = () => {
               dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780"
         >
           <Header title="Edit Runs" />
-
-          <div className="tracking-wide font-normal text-base mt-5 grid grid-cols-3 gap-4 content-around">
-          <label className="tracking-wide text-base font-bold">
-            Select Runs by Area:
-            <select
-              required
-              className="appearance-none font-normal w-52 bg-gray-200 text-gray-700 border border-gray-300 rounded py-2 px-3 mb-3 grid grid-cols-3 gap leading-tight focus:outline-none focus:bg-white"
-              id="area"
-              type="text"
-              onChange={(e) => getThisRun(e.target.value)}
-            >
-              {runsData.map((run) => (
-                <option key={run.area} value={run.area}>
-                  {run.area}
-                </option>
-              ))}
-            </select>
-          </label>
+          <div className="flex justify-between">
+            <p className="font-semibold text-3xl"></p>
+            <div className="flex flex-wrap items-center gap-4"></div>
+          </div>
           <form onSubmit={(e) => submit(e, thisRunData._id)}>
+            <div className="tracking-wide font-normal text-base  grid grid-cols-3 gap-4 content-around">
+              <label className="tracking-wide text-base font-bold">
+                Select Runs by Area:
+                <select
+                  required
+                  className="appearance-none font-normal w-52 bg-gray-200 text-gray-700 border border-gray-300 rounded py-2 px-3 mb-3 grid grid-cols-3 gap leading-tight focus:outline-none focus:bg-white"
+                  id="area"
+                  type="text"
+                  onChange={(e) => getThisRun(e.target.value)}
+                >
+                  {runsData.map((run) => (
+                    <option key={run.area} value={run.area}>
+                      {run.area}
+                    </option>
+                  ))}
+                </select>
+              </label>
+
               <label className="tracking-wide font-normal text-base">
                 Route Name:
                 <input
@@ -119,28 +123,28 @@ const EditRuns = () => {
                   onChange={(e) => setNewArea(e.target.value)}
                 ></input>
               </label>
-              <button
-                className="static h-8 px-6 ml-64 text-sm bg-blue-500 hover:bg-blue-700 text-white transition-colors duration-15 rounded-lg focus:shadow-outline"
-                type="submit"
-              >
-                Save
-              </button>
-              <button
-                className="h-8 ml-16 mb-4 px-4 text-sm bg-red-400 hover:bg-red-500 text-white transition-colors duration-15 rounded-lg focus:shadow-outline"
-                onClick={(e) => deleteRun(thisRunData._id, e)}
-              >
-                Delete
-                </button>
-              </form>
-               </div>
+            </div>
+            <button
+              className="mt-2 static h-8 px-6 ml-64 text-sm bg-blue-500 hover:bg-blue-700 text-white transition-colors duration-15 rounded-lg focus:shadow-outline"
+              type="submit"
+            >
+              Save
+            </button>
+            <button
+              className="h-8 ml-16 mb-4 px-4 text-sm bg-red-400 hover:bg-red-500 text-white transition-colors duration-15 rounded-lg focus:shadow-outline"
+              onClick={(e) => deleteRun(thisRunData._id, e)}
+            >
+              Delete
+            </button>
+          </form>
           <Link
             to="/runs"
             className="flex justify-center font-medium file:mt-8 text-blue-500 hover:text-blue-700 "
           >
             Back to Runs Lists
           </Link>
-            </div>
-            </div>
+        </div>
+      </div>
     </>
   );
 };
