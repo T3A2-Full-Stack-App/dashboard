dc9f748754f072e257e0d38175753fbd182a40ec
DamonJohnson <85825667+DamonJohnson@users.noreply.github.com>
2022-07-22 20:51:23 +1000

    fixed navbar issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from "react";
+import React, { useContext } from "react";
 import { AiOutlineMenu } from "react-icons/ai";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 import { useStateContext } from "../contexts/ContextProvider";
